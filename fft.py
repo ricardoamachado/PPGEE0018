a566e37d6a4814daa4d6d5f2022c5c2e4462c9d0
Ricardo Machado <72943956+ricardoamachado@users.noreply.github.com>
2025-10-22 13:37:05 -0300

    FFT Inicio

---
+++ fft.py
@@ -0,0 +1,13 @@
+import numpy as np
+def fft(x:list[complex]) -> list[complex]:
+    pass
+
+
+def main():
+    x = [1, 2, 3, 4]
+    fft_result = fft(x)
+    print("FFT Result:", fft_result)
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ppgee0018!")
-
-
-if __name__ == "__main__":
-    main()
